b755396040105861231dffdbdd76b6cfc7d54fad
Dina Nashaat <dina.nashaat@gmail.com>
2019-10-02 03:08:50 +0200

    add missing function to main in task 0 and update readme with python version

--- investigating_texts_calls/Readme.md
+++ investigating_texts_calls/Readme.md
@@ -1,11 +1,12 @@
 ## How to run the project:
+__Note__: Project runs with Python 3.7.4
 
-- `python main.py all` : Runs all tasks
-- `python main.py task3A` : Runs a specific task
-- `python main.py task1 task2` : Runs multiple tasks
+- `python3 main.py all` : Runs all tasks
+- `python3 main.py task3A` : Runs a specific task
+- `python3 main.py task1 task2` : Runs multiple tasks
 
 ## Available Tasks:
 `task0`, `task1`, `task2`, `task3A`, `task3B`, `task4`, `all`
 
 You can also run available Task3Test through: 
-`python -m unittest Task3Test`
+`python3 -m unittest Task3Test`
--- investigating_texts_calls/main.py
+++ investigating_texts_calls/main.py
@@ -13,31 +13,39 @@ def main():
     print('Task 0')
     print('======================')
     printFirstTextRecord()
+    print('\n')
+    printLastCallRecord()
+    print('\n')
 
   if('task1' in args or 'all' in args):
     print('Task 1')
     print('======================')
     printTelephoneNumbersCount()
+    print('\n')
 
   if('task2' in args or 'all' in args):
     print('Task 2')
     print('======================')
     printLongestTimeOnPhone()
+    print('\n')
 
   if('task3A' in args or 'all' in args):
     print('Task 3 - Part A')
     print('======================')
     codesCalledByBangalore()
+    print('\n')
   
   if('task3B' in args or 'all' in args):
     print('Task 3 - Part B')
     print('======================')
     bangaloreToBangaloreCallPercent()
+    print('\n')
   
   if('task4' in args or 'all' in args):
     print('Task 4')
     print('======================')
     identifyPossibleTelemarketers()
+    print('\n')
   
 if __name__== "__main__":
   main()
