d7b3c3688ef1a40c71f66dc66e56e0c2f4eed0ac
Dina Nashaat <dina.nashaat@gmail.com>
2019-11-10 06:37:36 +0200

    get all files with suffix .c in all subdirectories

---
+++ show_me_the_data_structures/File_Recursion.py
@@ -0,0 +1,29 @@
+import os
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    paths_list = []
+    for entry in os.listdir(path):
+        full_path = path + '/' + entry
+        if os.path.isdir(full_path):
+            paths_list = paths_list + find_files(suffix, full_path)
+        else:
+            if (entry.endswith(suffix)):
+                paths_list.append(entry)
+    return paths_list
+    
+print(find_files('.c', './testdir'))
